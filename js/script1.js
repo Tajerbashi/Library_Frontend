7d61573eb747287e421421d98f6ad86cea36ce6c
Tajerbashi <kamrantajerbashi@gmail.com>
2023-01-21 15:48:35 +0330

    Make Modal for Panel Item and design is Completed

--- js/script1.js
+++ js/script1.js
@@ -1,7 +1,5 @@
 var side=document.getElementById("nav");
 
-
-
 side.addEventListener("click",(event)=>{
     if(event.target.tagName === "UL" || event.target.tagName==="DIV"){
         if(side.classList.contains("close")){
