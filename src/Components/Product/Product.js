7ea1eb794897b311c12c9e5af40aace772a20b92
Tajerbashi <kamrantajerbashi@gmail.com>
2023-04-05 01:37:28 +0430

    Event Handler with point function in the component

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,8 +1,15 @@
 import './Product.css'
 
 const Product = ({title}) => {
+    const ShowInfo = () => {
+        alert("Product is : "+ title);
+        console.log("Product is : ", title);
+    }
     return (
-        <div class='product-container'>{title}</div>
+        <div className='product-container'>
+            <div>{title}</div>
+            <button className='btn' onClick={ShowInfo}>Click</button>
+        </div>
     )
 }
 
