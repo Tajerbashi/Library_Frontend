7682779128711597ed77a9aad05cb4afbfde1a53
Tajerbashi <kamrantajerbashi@gmail.com>
2023-04-05 00:56:39 +0430

    Used JSX in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = React.createElement
-(
-    'div',
-    {id:"app",className:"container"},
-    'Welcome to React Application'
-);
 
+// -------------------  Older Version of React -------------------
+// const element = React.createElement
+// (
+//         'div',
+//         {id:"app",className:"container"},
+//         'Welcome to React Application'
+//     );
+
+// -------------------  New Version of React -------------------
+const element = <div id="app" className="container">Welcome to React Application</div>
 ReactDOM.render(element, document.getElementById("root"));
