88860cee548831783375e72eeb6f35ededd7dbbc
Tajerbashi <kamrantajerbashi@gmail.com>
2023-04-05 01:48:09 +0430

    Make Dynamic Value for alert and log from input with event handler Click

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,12 +2,13 @@ import './Product.css'
 
 const Product = ({title}) => {
     const ShowInfo = () => {
-        alert("Product is : "+ title);
+        alert("Product is : "+ document.getElementById("inputer").value);
         console.log("Product is : ", title);
     }
     return (
         <div className='product-container'>
             <div>{title}</div>
+            <input type='text' className='inputer' id='inputer' />
             <button className='btn' onClick={ShowInfo}>Click</button>
         </div>
     )
