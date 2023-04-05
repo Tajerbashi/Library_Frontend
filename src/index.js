92a5ff52f11d32f594974af66756f77cdea34e0d
Kamran Tajerbashi <89404392+KTajerbashi@users.noreply.github.com>
2023-04-05 14:22:09 +0330

    React 18 Syntax

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import ReactDOM from "react-dom";
-import App from './App'
-import './index.css'
+//    ========================= : React 18 : =========================
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot( document.getElementById("root")).render(<App />);
+//  OR
+//    ========================= : React 17 : =========================
+// import './index.css'
 // in index.js just import App component because it's will be the parent component and all components will be children of App component so import in App.js component
 
 // -------------------  Older Version of React -------------------
+// import ReactDOM from "react-dom";
 // const element = React.createElement
 // (
 //         'div',
 //         {id:"app",className:"container"},
 //         'Welcome to React Application'
 //     );
-
-// -------------------  New Version of React -------------------
-// const element = <div id="app" className="container">Welcome to React Application</div>
 // ReactDOM.render(element, document.getElementById("root"));
-
-
-ReactDOM.render(<App />, document.getElementById("root"))
+                                                             
+// -------------------  New Version of React -------------------
+// import ReactDOM from "react-dom";
+// import App from './App'
+                                                             
+// ReactDOM.render(<App />, document.getElementById("root"))                                                          
