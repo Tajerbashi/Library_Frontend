b5861eefa7b8f4096005c6a88b646fca03ded03a
Tajerbashi <kamrantajerbashi@gmail.com>
2023-05-05 17:57:34 +0330

    Make one line condition expression in react but it usage is just on Class Components and render (){} Function and its not worked on function components because function does not have any render default method

--- src/Components/App/app.js
+++ src/Components/App/app.js
@@ -1,23 +1,122 @@
-import {React} from "react"
+import {React, useState} from "react"
 import './app.css'
 
 import MainHeader from "../header/header";
 import MainNav from "../nav/nav";
 import Product from "../Product/Product";
-
+import { render } from "@testing-library/react";
 const App = () => {
+    console.log("Starting");
+    const [productsState,setProductsState] = useState({
+        products:[
+            {
+                title: "شما نسخه برتر هستید !",
+                price: "2,500,000"
+            },
+            {
+                title: "جادوی فکر بزرگ",
+                price: "1,500,000"
+            },
+            {
+                title: "چهار اثر فلورانس",
+                price: "1,200,000"
+            },
+            {
+                title: "اثر مرکب",
+                price: "980,000"
+            },
+            {
+                title: "قدرت نامحدود",
+                price: "1,100,000"
+            },
+            {
+                title: "365 گام موفقیت",
+                price: "1,750,000"
+            },
+        ],
+        ShowProducts : true
+    });
+    const ChangeTitle = (event,index) => {
+        console.log('====================================');
+        console.log(event.target.value);
+        console.log('====================================');
+    };
+    const ChangePrice = (event,index) => {
+        console.log('====================================');
+        console.log(event.target.value);
+        console.log('====================================');
+    };
+    const ShowHandler = () => {
+        productsState.ShowProducts = productsState.ShowProducts ? false : true;
+        setProductsState.ShowProducts = setProductsState.ShowProducts ? false : true;
+        console.log('====================================');
+        console.log("SetState : ",setProductsState.ShowProducts);
+        console.log("State : ",productsState.ShowProducts);
+        console.log('====================================');
+    };
+    let btn = {
+        padding: '1rem',  
+        margin: '0.2rem auto',
+        borderRadius: '5rem',
+        border: 'none',
+        textAlign: 'center',
+        alignItems: 'center',
+        background: '#000',
+        color: 'wheat',
+        boxShadow : '0 0 10px 5px #0005',
+    };
     return (
         <div>
             <MainHeader />
             <MainNav />
-            <div className="app-product-list-div">
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-            </div>
+            <button style={btn} onClick={ShowHandler}>Hide/Show Products</button>
+            {
+                productsState.ShowProducts ? (
+                    <div className="app-product-list-div">
+                        <Product
+                                title={productsState.products[0].title}
+                                price={productsState.products[0].price}
+
+                                ChangeTitle={ChangeTitle}
+                                ChangePrice={ChangePrice}
+                                 />
+                        <Product
+                                title={productsState.products[1].title}
+                                price={productsState.products[1].price}
+
+                                ChangeTitle={ChangeTitle}
+                                ChangePrice={ChangePrice}
+                                 />
+                        <Product
+                                title={productsState.products[2].title}
+                                price={productsState.products[2].price}
+
+                                ChangeTitle={ChangeTitle}
+                                ChangePrice={ChangePrice}
+                                 />
+                        <Product
+                                title={productsState.products[3].title}
+                                price={productsState.products[3].price}
+
+                                ChangeTitle={ChangeTitle}
+                                ChangePrice={ChangePrice}
+                                 />
+                        <Product
+                                title={productsState.products[4].title}
+                                price={productsState.products[4].price}
+
+                                ChangeTitle={ChangeTitle}
+                                ChangePrice={ChangePrice}
+                                 />
+                        <Product
+                                title={productsState.products[5].title}
+                                price={productsState.products[5].price}
+
+                                ChangeTitle={ChangeTitle}
+                                ChangePrice={ChangePrice}
+                                 />
+                    </div> ) : null
+            }
         </div>
     );
 }
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,10 +1,20 @@
 import './Product.css'
-const Product = () => {
+const Product = (props) => {
     return (
         <div className='product-container'>
-            <h6 className='product-title'>عنوان محصول</h6>
-            <p className='product-price'>250,000 تومان</p>
+            <h6 className='product-title'>{props.title}</h6>
+            <p className='product-price'>{props.price} تومان</p>
             <button className='product-button'>دیدن</button>
+            <div className='product-configs-con'>
+                <div className='product-config-item'>
+                    <input  className='product-inputer-1' placeholder='عنوان جدید وارد کنید ...' onChange={props.ChangeTitle} />
+                    <button className='btn-t' onClick={props.SaveTitle}>+</button>
+                </div>
+                <div className='product-config-item'>
+                    <input className='product-inputer-2' placeholder='قیمت جدید وارد کنید ...' onChange={props.ChangePrice} />
+                    <button className='btn-p' onClick={props.SavePrice}>+</button>
+                </div>
+            </div>
         </div>
     );
 }
