aa32fc680eb0df8d4c463f8902ba0d044aed339b
Tajerbashi <kamrantajerbashi@gmail.com>
2023-05-05 22:47:09 +0330

    Last Changed

--- src/Components/App/app.js
+++ src/Components/App/app.js
@@ -10,52 +10,55 @@ const App = () => {
             {
                 title: "شما نسخه برتر هستید !",
                 price: "2,500,000",
-                index: 0
+                id: 0
             },
             {
                 title: "جادوی فکر بزرگ",
                 price: "1,500,000",
-                index: 1
+                id: 1
             },
             {
                 title: "چهار اثر فلورانس",
                 price: "1,200,000",
-                index: 2
+                id: 2
             },
             {
                 title: "اثر مرکب",
                 price: "980,000",
-                index: 3
+                id: 3
             },
             {
                 title: "قدرت نامحدود",
                 price: "1,100,000",
-                index: 4
+                id: 4
             },
             {
                 title: "365 گام موفقیت",
                 price: "1,750,000",
-                index: 5
+                id: 5
             },
         ],
         ShowProducts : true
     });
+
     const ChangeTitle = (e,index) => {
         let data = productsState.products;
         data[index].title = e.target.value;
         setProductsState({products: data,ShowProducts : true});
         // setProductsState(data);
     };
+
     const ChangePrice = (e,index) => {
         let data = productsState.products;
         data[index].price = e.target.value;
         setProductsState({products: data,ShowProducts : true});
     };
+
     const ShowHandler = () => {
         let flag = productsState.ShowProducts ? false : true;
         setProductsState({products: productsState.products,ShowProducts : flag});
-
     };
+
     const DeleteProduct = (index) =>{
         const data = [...productsState.products]
         // let data = productsState.products;
