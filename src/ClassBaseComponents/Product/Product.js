67ace1243e36f03ac442fe2fac0748426e2d9fd1
Tajerbashi <kamrantajerbashi@gmail.com>
2023-05-11 23:53:44 +0330

    Change product and productList Components to class Component and used from this in the components

--- src/ClassBaseComponents/Product/Product.js
+++ src/ClassBaseComponents/Product/Product.js
@@ -1,27 +1,31 @@
+import React, {Component} from 'react';
+
 import './Product.css'
-const Product = (props) => {
-    console.log("Product Log");
-    return (
-        <div className='product-container'>
-            <h6 className='product-title'>{props.title}</h6>
-            <p className='product-price'>{props.price} تومان</p>
-            <button className='product-button'>دیدن</button>
-            <div className='product-configs-con'>
-                <div className='product-config-item'>
-                    <input  className='product-inputer-1' placeholder='عنوان جدید وارد کنید ...' onChange={props.ChangeTitle} />
-                    <button className='btn-t' onClick={props.SaveTitle}>+</button>
+class Product extends Component {
+    render(){
+        console.log("Product Log");
+        return (
+            <div className='product-container'>
+                <h6 className='product-title'>{this.props.title}</h6>
+                <p className='product-price'>{this.props.price} تومان</p>
+                <button className='product-button'>دیدن</button>
+                <div className='product-configs-con'>
+                    <div className='product-config-item'>
+                        <input  className='product-inputer-1' placeholder='عنوان جدید وارد کنید ...' onChange={this.props.ChangeTitle} />
+                        <button className='btn-t' onClick={this.props.SaveTitle}>+</button>
+                    </div>
+                    <div className='product-config-item'>
+                        <input type='number' min='0' max='9999999' className='product-inputer-2 text-center' placeholder='قیمت جدید وارد کنید ...' onChange={this.props.ChangePrice} />
+                        <button className='btn-p' onClick={this.props.SavePrice}>+</button>
+                    </div>
+                    <button className='btn text-danger mt-1' 
+                            onClick={this.props.Delete}>
+                                حذف محصول
+                    </button>
                 </div>
-                <div className='product-config-item'>
-                    <input type='number' min='0' max='9999999' className='product-inputer-2 text-center' placeholder='قیمت جدید وارد کنید ...' onChange={props.ChangePrice} />
-                    <button className='btn-p' onClick={props.SavePrice}>+</button>
-                </div>
-                <button className='btn text-danger mt-1' 
-                        onClick={props.Delete}>
-                            حذف محصول
-                </button>
+                {false ? this.props.index + 1 : null}
             </div>
-            {false ? props.index + 1 : null}
-        </div>
-    );
+        );
+    }
 }
 export default Product;
--- src/ClassBaseComponents/ProductList/ProductList.js
+++ src/ClassBaseComponents/ProductList/ProductList.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Product from "../Product/Product";
 import './ProductList.css'
-const ProductList = (props) => {
-    console.log("Product Log");
-            return props.products.map((item,index) =>{
+class ProductList extends Component {
+    render(){
+        console.log("Product Log");
+            return this.props.products.map((item,index) =>{
                 return(
                     <Product 
-                            Delete = {() => props.Delete(index)}
+                            Delete = {() => this.props.Delete(index)}
                             title ={item.title} 
                             price ={item.price} 
                             index = {item.index}
                             key={item.id}
-                            ChangeTitle = {(e) => props.ChangeTitle(e,item.id)} 
-                            ChangePrice = {(e) => props.ChangePrice(e,item.id)} 
+                            ChangeTitle = {(e) => this.props.ChangeTitle(e,item.id)} 
+                            ChangePrice = {(e) => this.props.ChangePrice(e,item.id)} 
                     />
                 )
             });
+    }
                     
 }
 export default ProductList;
