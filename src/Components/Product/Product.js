ab58c4b79f7b77ae6dfb25f00441ccc93ba7395a
Tajerbashi <kamrantajerbashi@gmail.com>
2023-05-19 17:50:12 +0330

    AuthContext in Product.js

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,35 +1,63 @@
 import { useEffect } from 'react';
 import './Product.css'
 import Dynamic from '../../HOC/Dynamic';
+import AuthContext from '../Context/Auth-Context'
 const Product = (props) => {
-    useEffect(()=>{
-        return ()=>{
+    useEffect(() => {
+        return () => {
             console.log('::This is From Clean Up In Product.js::');
         }
-    },[]);
+    }, []);
     return (
         <div className='product-container'>
-            { props.IsAuth ? 
-            <div>
-            <h6 className='product-title'>{props.title}</h6>
-            <p className='product-price'>{props.price} تومان</p>
-            <button className='product-button'>دیدن</button>
-            <div className='product-configs-con'>
-                <div className='product-config-item'>
-                    <input  className='product-inputer-1' placeholder='عنوان جدید وارد کنید ...' onChange={props.ChangeTitle} />
-                    <button className='btn-t' onClick={props.SaveTitle}>+</button>
-                </div>
-                <div className='product-config-item'>
-                    <input type='number' min='0' max='9999999' className='product-inputer-2 text-center' placeholder='قیمت جدید وارد کنید ...' onChange={props.ChangePrice} />
-                    <button className='btn-p' onClick={props.SavePrice}>+</button>
-                </div>
-                <button className='btn text-danger mt-1' 
-                        onClick={props.Delete}
+
+            <AuthContext.Consumer>
+                {
+                    (context) => context.auth ?
+                        <div>
+                            <h6 className='product-title'>{props.title}</h6>
+                            <p className='product-price'>{props.price} تومان</p>
+                            <button className='product-button'>دیدن</button>
+                            <div className='product-configs-con'>
+                                <div className='product-config-item'>
+                                    <input className='product-inputer-1' placeholder='عنوان جدید وارد کنید ...' onChange={props.ChangeTitle} />
+                                    <button className='btn-t' onClick={props.SaveTitle}>+</button>
+                                </div>
+                                <div className='product-config-item'>
+                                    <input type='number' min='0' max='9999999' className='product-inputer-2 text-center' placeholder='قیمت جدید وارد کنید ...' onChange={props.ChangePrice} />
+                                    <button className='btn-p' onClick={props.SavePrice}>+</button>
+                                </div>
+                                <button className='btn text-danger mt-1'
+                                    onClick={props.Delete}
+                                >حذف محصول</button>
+                            </div>
+                            {false ? props.index + 1 : null}
+                        </div>
+                        :
+                        null
+                }
+            </AuthContext.Consumer>
+            {/* {props.IsAuth ?
+                <div>
+                    <h6 className='product-title'>{props.title}</h6>
+                    <p className='product-price'>{props.price} تومان</p>
+                    <button className='product-button'>دیدن</button>
+                    <div className='product-configs-con'>
+                        <div className='product-config-item'>
+                            <input className='product-inputer-1' placeholder='عنوان جدید وارد کنید ...' onChange={props.ChangeTitle} />
+                            <button className='btn-t' onClick={props.SaveTitle}>+</button>
+                        </div>
+                        <div className='product-config-item'>
+                            <input type='number' min='0' max='9999999' className='product-inputer-2 text-center' placeholder='قیمت جدید وارد کنید ...' onChange={props.ChangePrice} />
+                            <button className='btn-p' onClick={props.SavePrice}>+</button>
+                        </div>
+                        <button className='btn text-danger mt-1'
+                            onClick={props.Delete}
                         >حذف محصول</button>
-            </div>
-            {false ? props.index + 1 : null}
-            </div>
-            : null}
+                    </div>
+                    {false ? props.index + 1 : null}
+                </div>
+                : null} */}
         </div>
     );
 }
