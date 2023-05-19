417b53e0f9e1ad52f66d0e3ed9eb6c05486e8669
Tajerbashi <kamrantajerbashi@gmail.com>
2023-05-19 19:12:21 +0330

    Start New Project Library in React

--- src/ClassBaseComponents/Context/auth-Context.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const authContext = React.createContext({
-    auth:false,
-    login: () => {},
-});
-
-export default authContext;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './Components/App/app';
-import AppClass from './ClassBaseComponents/App/app';
+// import AppClass from './ClassBaseComponents/App/app';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.rtl.css'
 import './index.css'
