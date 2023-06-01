4e6c7bebc2255ecb5e9dfbea8ef7880d4ad6efc2
KTajerbsahi <kamrantajerbashi@gmail.com>
2023-06-02 02:07:59 +0330

    make services for mysql and mssqlserver and mongodb

--- src/Services/Server/MongoDB/Connection.js
+++ src/Services/Server/MongoDB/Connection.js
@@ -99,18 +99,6 @@
 ////////////////////////////////////////////////////////////////
 
 
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
 const mysql = require('mysql');
  
 const connection = mysql.createConnection({
