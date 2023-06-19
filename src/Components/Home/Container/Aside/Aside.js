99f6d22f7f0ef2a69ec463de50e1b445f54bccdc
KTajerbsahi <kamrantajerbashi@gmail.com>
2023-06-19 23:55:00 +0330

    Create Admin Component to handle Admin Panel

--- src/Components/Home/Container/Aside/Aside.js
+++ src/Components/Home/Container/Aside/Aside.js
@@ -1,7 +1,11 @@
 import './Aside.css'
+import {BrowserRouter as Router,Link} from 'react-router-dom'
 const Aside = () => {
     return (
         <div className='DefaultContainer Aside-Container'>
+            <ul>
+                <li>مدیریت کاربران</li>
+            </ul>
         </div>
     );
 }
