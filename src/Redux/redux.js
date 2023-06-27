8bd8a0644125aa2a5967e08aec8d86577c51c43a
KTajerbsahi <kamrantajerbashi@gmail.com>
2023-06-27 23:05:34 +0330

    Use Dispatch and Redux Methods

--- src/Redux/redux.js
+++ src/Redux/redux.js
@@ -7,9 +7,43 @@ const initialState = {
 }
 
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC') {
+        return ({
+            ...state,
+            counter: state.counter + 2
+        });
+    } else if (action.type === 'ADD') {
+        return ({
+            ...state,
+            counter: state.counter + action.value
+        });
+    } else if (action.type === 'DEL') {
+        return ({
+            ...state,
+            counter: state.counter - action.value
+        });
+    }
     return state;
 }
 
 const store = createStore(rootReducer);
+
+// console.log(store.getState());
+
+
+
+store.dispatch({
+    type: 'INC',
+});
+
+store.dispatch({
+    type: 'ADD',
+    value: 10
+});
+
+store.dispatch({
+    type: 'DEL',
+    value: 1
+});
 
 console.log(store.getState());
