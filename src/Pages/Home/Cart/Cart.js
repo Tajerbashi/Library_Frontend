eb13ff84fdb40f7a4b03177886961a220a364d16
KTajerbsahi <kamrantajerbashi@gmail.com>
2023-07-04 00:37:31 +0330

    Show Products in Cart

--- src/Pages/Home/Cart/Cart.js
+++ src/Pages/Home/Cart/Cart.js
@@ -10,28 +10,60 @@ const Cart = () => {
   const productId = params.id;
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-  console.log(cartItems);
+
+  const removeFromCartHandler = (id) => {
+    console.log("ID : ", id);
+  }
+
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId));
     }
   }, [dispatch, productId]);
-  return (
-    <div className='CartContainer'>
-      <Row>
-        <Col md={8}>
-          <h2>سبد خرید</h2>
-          {
-            cartItems.length === 0
-              ? <p>سبد خرید خالی است</p>
-              : <ListGroup variant='flush'></ListGroup>
-          }
-        </Col>
-        <Col md={4}>
 
-        </Col>
-      </Row>
-    </div>
+  const List = cartItems.map((item) => {
+    return (
+      <ListGroup.Item key={item.product}>
+        <Row>
+          <Col md={2}>
+            <Image src={item.image} alt={item.name} fluid rounded />
+          </Col>
+          <Col md={3}>
+            {item.name}
+          </Col>
+          <Col md={2}>
+            {item.price}
+          </Col>
+          <Col md={2}>
+            <Button
+              type='button'
+              variant='light'
+              onClick={() => removeFromCartHandler(item.product)}
+            >
+              <i className='fa fa-trash'></i>
+            </Button>
+          </Col>
+        </Row>
+      </ListGroup.Item>
+    );
+  })
+  return (
+    <Row className='CartContainer'>
+      <Col md={8}>
+        <h2>سبد خرید</h2>
+        {
+          cartItems.length === 0
+            ? (<p>سبد خرید خالی است</p>)
+            : (<ListGroup variant='flush'>
+              {
+                List
+              }
+            </ListGroup>
+            )}
+      </Col>
+      <Col md={4}>
+      </Col>
+    </Row>
   )
 }
 
