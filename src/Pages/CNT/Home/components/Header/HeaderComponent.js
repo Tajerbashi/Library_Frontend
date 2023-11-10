2bd98190efb14c4737e0c4eff66bbcdfc9ec2dd6
Tajerbashi <kamrantajerbashi@gmail.com>
2023-11-10 11:07:52 +0330

    Start New Project

--- src/Pages/CNT/Home/components/Header/HeaderComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function HeaderComponent() {
-  return (
-    <header className="container d-flex flex-wrap justify-content-center ">
-    </header>
-  )
-}
